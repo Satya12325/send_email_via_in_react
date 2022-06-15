a87f4fdc1cd5c68e706f11d9c5c2c489beafefd4
Satyabrata Das <satyabrata_pw1_135@masai.school>
2022-06-15 18:46:51 +0530

    send mail to email with the help of email js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import emailjs from '@emailjs/browser';
+import React, { useRef } from 'react';
 
 function App() {
+
+  const form = useRef();
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_hg3way6', 'template_c4iy521', form.current, '022NlpnUGTQGoa6oT')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+         <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="from_name" /><br/><br/>
+      <label>Email</label>
+      <input type="email" name="user_email" /><br/><br/>
+      <label>Subject</label>
+      <textarea name="subject" /><br/><br/>
+      <label>Message</label>
+      <textarea name="message" /><br/><br/>
+      <input type="submit" value="Send" />
+    </form>
+
+
     </div>
   );
 }
